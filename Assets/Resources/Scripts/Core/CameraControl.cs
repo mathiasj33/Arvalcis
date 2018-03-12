fb2359e4dfaf5f93f6716b23f48cedf5e7b86879
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2018-03-12 23:26:52 +0100

    Made the player transform private
    It can therefore not be set via drag and drop. This change was necessary due to strange behavior in the editor.

--- Assets/Resources/Scripts/Core/CameraControl.cs
+++ Assets/Resources/Scripts/Core/CameraControl.cs
@@ -7,12 +7,13 @@ public class CameraControl : MonoBehaviour
     public float height;
     [Range(0.0f, 90)]
     public float angle;
-    public Transform player;
-
     public float zDistance;
+
+    private Transform player;
 
     void Start()
     {
+        player = GameObject.Find("Player").transform;
         transform.position = new Vector3(player.position.x, height, player.position.z);
         transform.eulerAngles = new Vector3(angle, transform.eulerAngles.y, transform.eulerAngles.z);
     }
@@ -24,6 +25,7 @@ public class CameraControl : MonoBehaviour
 
     void OnValidate()
     {
+        if(player == null) return;
         transform.position = new Vector3(player.position.x, height, player.position.z - zDistance);
         transform.eulerAngles = new Vector3(angle, transform.eulerAngles.y, transform.eulerAngles.z);
     }
