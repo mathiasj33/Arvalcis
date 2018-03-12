2f3ed091392eae6fa84543f61b5ffde43a64e40e
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2018-03-12 23:58:15 +0100

    Finished #11: Cleanup up of the EnemyAnimationControl
    Refactored the class and added functionality to randomly rotate the blood texture and choose a random blood texture.

--- Assets/Resources/Scripts/Core/EnemyAnimationControl.cs
+++ Assets/Resources/Scripts/Core/EnemyAnimationControl.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class EnemyAnimationControl : MonoBehaviour
 {
     public GameObject chest;
-	public GameObject blood;
+    public GameObject blood;
 
     private CharacterController characterController;
     private Animator animator;
 
-	private Invoker invoker;
+    private Invoker invoker;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-		invoker = GameObject.Find("Main").GetComponent<Invoker>();
+        invoker = GameObject.Find("Main").GetComponent<Invoker>();
     }
 
     void Update()
@@ -26,27 +26,36 @@ public class EnemyAnimationControl : MonoBehaviour
 
     public void PlayDeathAnim(Vector3 dir)
     {
+        ActivateRagdoll();
+        chest.GetComponent<Rigidbody>().AddForce(dir * 100, ForceMode.Impulse);
+        invoker.Invoke(1f, ShowBlood);
+        Destroy(this);
+    }
+
+    private void ActivateRagdoll()
+    {
         Destroy(characterController);
         Destroy(GetComponent<BoxCollider>()); //this collider is only used to provide a better hitbox
         animator.enabled = false;
-        // blood.transform.position = GameObject.Find("BloodPos").transform.position;
-        // blood.transform.rotation = GameObject.Find("BloodPos").transform.rotation;
-        // blood.Play();
         foreach (Rigidbody r in GetComponentsInChildren<Rigidbody>())
         {
             r.isKinematic = false;
         }
-        chest.GetComponent<Rigidbody>().AddForce(dir * 100, ForceMode.Impulse);
-		invoker.Invoke(1f, ShowBlood);
-        //Destroy(this);
     }
 
     private void ShowBlood()
     {
-		blood.transform.parent = null;
-		blood.transform.position = new Vector3(blood.transform.position.x, .05f, blood.transform.position.z);
-        blood.transform.rotation = Quaternion.Euler(0, 0, 0);
+        blood.transform.parent = null;
+        blood.transform.position = new Vector3(blood.transform.position.x, .05f, blood.transform.position.z);
+        blood.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        blood.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", LoadRandomBloodTexture());
         blood.AddComponent<ScaleBloodControl>();
         blood.SetActive(true);
+    }
+
+    private Texture LoadRandomBloodTexture()
+    {
+        int texID = Random.Range(1, 3);
+        return Resources.Load<Texture>("Textures/BloodSplats/Splat" + texID);
     }
 }
